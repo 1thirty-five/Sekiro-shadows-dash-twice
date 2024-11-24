3a88e8ca21e768d60b6bdadd7ccee2ce38034d98
1thirtyfive <holofoil79@gmail.com>
2024-11-25 03:33:42 +0530

    04

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import pygame
 import sys
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image,load_images1
+from scripts.utils import load_image,load_images1a
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
+
 
 class game:
     def __init__(self):
@@ -24,23 +26,35 @@ class game:
             'grass':load_images1('tiles/grass'),
             'large_decor':load_images1('tiles/large_decor'),
             'stone':load_images1('tiles/stone'),
-            'player':load_image('entities/player.png')
+            'player':load_image('entities/player.png'),
+            'background':load_image('background.png'),
+            'clouds': load_images1('clouds'),
             }
         
-        self.player = PhysicsEntity(self, 'player', (50,50), (8,15))#player render
+        self.clouds= Clouds(self.assets['clouds'], count=16)
+        
+        self.player = PhysicsEntity(self, 'player', (50,50), (8,15))#player render 
         
         self.tilemap = Tilemap(self, tile_size=16)
         
-        self.scrol = 
+        self.scroll = [0,0]
+        
         
     def run(self):
         while True:
-            self.display.fill((14,219,248))     
+            self.display.blit(self.assets['background'],(0,0))
             
-            self.tilemap.render(self.display) 
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) # /2 ensures player is on the center of the screen  and /30 is a random value to ensure that camera catches up
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) 
+            render_scroll = (int(self.scroll[0]),int(self.scroll[1]))
+            
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+            
+            self.tilemap.render(self.display,offset=self.scroll) 
             
             self.player.update(self.tilemap,(self.movement[1] - self.movement[0] ,0))
-            self.player.render(self.display)
+            self.player.render(self.display,offset=self.scroll)
             
             
             for event in pygame.event.get():
--- scripts/entities.py
+++ scripts/entities.py
@@ -32,7 +32,7 @@ class PhysicsEntity:
         self.pos[1] += frame_movement[1]
         entity_rect = self.rect()
         for rect in tilemap.physics_rects_around(self.pos):
-            if entity_rect.colliderect(rect):
+            if entity_rect.colliderect(rect):  
                 if frame_movement[1] > 0:
                     entity_rect.bottom = rect.top
                     self.collisions['down'] = True
