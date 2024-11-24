5382ae94b1e6031f26798dad8c41eea66414f276
1thirtyfive <holofoil79@gmail.com>
2024-11-25 01:27:57 +0530

    1

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,6 @@
+NEIGHBOR_OFFSET=[(-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(0,0),(-1,1),(0,1),(1,1)]
+PHYSICS_TILES = {'grass','stone'}
+
 class Tilemap:
     def __init__(self,game,tile_size=16):
         self.game = game
@@ -8,7 +11,23 @@ class Tilemap:
         for i in range(10):
             self.tilemap[str(3+i) + ';10'] = {'type': 'grass', 'variant':1, 'pos':(3+i,10)} 
             self.tilemap[';10' + str(i+5)] = {'type': 'stone', 'variant':1, 'pos':(10,i+5)} 
+            
+            
+    def tiles_around(self,pos):
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size)  )#integer div to chop off the remainder
+        for offset in NEIGHBOR_OFFSET:
+            check_loc=str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])   
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
     
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile
+            
+        
     def render(self,surf):
         for tile in self.offgrid_tiles:
             surf.blit(self.game.assets[tile['type' ]][tile['variant']], tile['pos'])
