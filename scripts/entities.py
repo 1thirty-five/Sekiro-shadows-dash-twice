ef8a6c28a45b06d9f8e7bd43b3e6d72ffbac231c
Aadit Narke <159511286+1thirty-five@users.noreply.github.com>
2024-11-25 11:01:29 +0530

    s

--- scripts/entities.py
+++ scripts/entities.py
@@ -13,6 +13,8 @@ class PhysicsEntity:
         self.anim_offset = (-3, -3)
         self.flip = False
         self.set_action('idle')
+
+        self.last_movement = [0,0]
     
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
@@ -98,8 +100,15 @@ class Player(PhysicsEntity):
         else:
             self.set_action('idle')
     
+
+        if self.velocity[0]>0:
+            self.velocity[0] = min(self.velocity[0] - 0.1,0)
+        else:
+            self.velocity[0] = min(self.velocity[0] + 0.1,0)
+                
+
     def jump(self):
-        if self.wall_slide:
+        if self.wallslide:
             if self.flip and self.last_movement[0] < 0:
                 self.velocity[0] = 3.5
                 self.velocity[1] = -2.5
