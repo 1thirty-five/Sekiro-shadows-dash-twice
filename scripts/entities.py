58dcdf0421c66de9b54b30b4d5e1b4501502dd81
Aadit Narke <159511286+1thirty-five@users.noreply.github.com>
2024-11-25 10:42:05 +0530

    d

--- scripts/entities.py
+++ scripts/entities.py
@@ -108,7 +108,8 @@ class Player(PhysicsEntity):
                 return True
             elif not self.flip and self.last_movement[0] > 0:
                 self.velocity[0] = -3.5
-                self.velocity[1] = -2.5                self.air_time = 5
+                self.velocity[1] = -2.5             
+                self.air_time = 5
                 self.jumps = max(0, self.jumps - 1)
                 return True
         
